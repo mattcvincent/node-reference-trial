34200e1824450b7b4a8d443ff849a6c520268fc7
Matt Vincent <mattcvincent@gmail.com>
2018-06-19 22:05:16 -0500

    done with node-reference-validation; passing tests :)

--- products/createProduct.js
+++ products/createProduct.js
@@ -2,9 +2,19 @@ const shortid = require('shortid');
 const AWS = require('aws-sdk');
 const documentClient = new AWS.DynamoDB.DocumentClient();
 const productsTableName = process.env.PRODUCTS_TABLE_NAME;
+const validateProduct = require('./validateProduct');
+
 
 module.exports = async function createProduct(ctx) {
     const product = ctx.request.body;
+
+    const validatonErrors = validateProduct(product);
+
+    if (validatonErrors) {
+        ctx.body = validatonErrors;
+        ctx.status = 400;
+        return;
+    }
 
     product.id = shortid.generate();
     await saveProduct(product);
